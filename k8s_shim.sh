42269b21b9cc08ae2338bf4ac14a0534fd485104
apollorion <joey@apollorion.com>
2023-06-29 09:28:09 -0400

    feat(secret): allow bypassing easy-k8s-secrets

--- k8s_shim.sh
+++ k8s_shim.sh
@@ -4,6 +4,12 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 ORIGINAL_KUBECTL=$(which kubectl)
 
+ez_k8s_log(){
+  if [ -n "$EZ_K8S_SECRETS_DEBUG" ]; then
+    echo "$@"
+  fi
+}
+
 kubectl(){
   PATH_TO_SECRET_EXECUTABLE="${SCRIPTPATH}/secret"
 
@@ -11,11 +17,20 @@ kubectl(){
     *"get"*"secret"* | *"edit"*"secret"* | *"create"*"secret"*)
         # ignore if "secrets" instead of "secret"
         # shellcheck disable=SC2199
-        if [[ "$@" == *"secrets"* ]]; then
-          echo "Ignoring easy k8s secrets command, string 'secrets' found in command"
-          $ORIGINAL_KUBECTL "$@"
+        if [[ "$@" == *"--no-easy-k8s-secrets "* ]]; then
+          ez_k8s_log "INFO: Ignoring easy k8s secrets command, '--no-easy-k8s-secrets' found in command"
+          # shellcheck disable=SC2001
+          # shellcheck disable=SC2207
+          # shellcheck disable=SC2006
+          removedPassthrough=(`echo "$@"| sed "s/--no-easy-k8s-secrets //"`)
+          $ORIGINAL_KUBECTL "${removedPassthrough[@]}"
         else
-          $PATH_TO_SECRET_EXECUTABLE "${@}"
+          if [[ "$@" == *"secrets"* ]]; then
+            ez_k8s_log "INFO: Ignoring easy k8s secrets command, string 'secrets' found in command"
+            $ORIGINAL_KUBECTL "${@}"
+          else
+            $PATH_TO_SECRET_EXECUTABLE "${@}"
+          fi
         fi
     ;;
     *)
